1939d8af89fbf73b5d68f4d8fdbddac770e4ba89
jy3321 <821535576@qq.com>
2020-03-15 22:27:43 +0800

    java刷题代码保存初始化

---
+++ src/Leetcode/interviewQuestion25Test.java
@@ -0,0 +1,14 @@
+package Leetcode;
+
+import Leetcode.ListNode;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class interviewQuestion25Test {
+
+    @Test
+    public void mergeTwoLists() {
+    }
+
+}
